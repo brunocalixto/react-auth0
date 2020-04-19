19af10d10cee417fb90e45eba83705d4abdd40ea
Bruno Calixto <bruno.calixto@b2wdigital.com>
2020-04-19 20:57:06 -0300

    add: save tokens in localStorage

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -15,4 +15,28 @@ export default class Auth {
   login = () => {
     this.auth0.authorize();
   };
+
+  handleAuthentication = (history) => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        this.history.push("/");
+      } else if (err) {
+        this.history.push("/");
+        alert(`Error: ${err.error}. Check the console for further details`);
+        console.log(err);
+      }
+    });
+  };
+
+  setSession = (authResult) => {
+    console.log(authResult);
+    // set the time that the access token will expire
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem("access_token", authResult.acessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", authResult.expiresAt);
+  };
 }
